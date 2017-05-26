6f3c0df058d0b54dd10cfa337305af3ff5e45fcc
Luiz Felix <lzcfelix@gmail.com>
2017-05-26 14:40:35 -0300

    Fixed [run_all.sh].

--- run_all.sh
+++ run_all.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-batches=(500, 1000 5000 10000 15000 20000 25000 30000)
-algorithms=('bubble' 'bubble_improved' 'heap' 'insertion' 'merge' 'quick' 'quick_median' 'selection'
-            'iquick' 'iquick_median')
+batches=(500 1000 5000 10000 15000 20000 25000 30000)
+algorithms=('bubble' 'bubble_improved' 'heap' 'insertion' 'merge' 'quick' 'quick_median' 'selection' 'iquick' 'iquick_median')
 modes=('r' 'a' 'd')
 
 for batch in "${batches[@]}"
 do
-    echo ${batch}
     for algorithm in "${algorithms[@]}"
     do
         for mode in "${modes[@]}"
         do
-            echo -e "\t ${algorithm} - ${mode}"
+            echo -e "\t ${batch} ${algorithm} - ${mode}"
             python runner.py -a=${algorithm} --mode=${mode} --size=${batch} --times=10 -f
         done
     done
